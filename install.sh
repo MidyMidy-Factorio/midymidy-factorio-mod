bc9ec97448ec95d583c8d089dbcf91d2628c9989
Nick <znhihgiasy@gmail.com>
2020-01-20 10:23:18 +0800

    installer deletes older mods

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@ name=midymidy-factorio-mod_$version
 mkdir $name
 cp -r *.lua info.json locale thumbnail.png $name
 zip -r $name.zip $name
+rm ~/.factorio/mods/midymidy-factorio-mod_*.zip
 cp $name.zip ~/.factorio/mods/
 rm -rf $name
