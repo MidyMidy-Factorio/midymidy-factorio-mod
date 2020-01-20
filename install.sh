c44dd476cc4780a208acf3ef1db4346b63a85045
Nick <znhihgiasy@gmail.com>
2020-01-20 10:30:24 +0800

    installs to server

--- install.sh
+++ install.sh
@@ -6,3 +6,6 @@ zip -r $name.zip $name
 rm ~/.factorio/mods/midymidy-factorio-mod_*.zip
 cp $name.zip ~/.factorio/mods/
 rm -rf $name
+if [[ $1 = "-u" ]]; then 
+    cat ~/.factorio/mods/$name.zip | ssh factorio@factorio.nicball.space "cat > /opt/factorio/mods/$name.zip"
+fi
